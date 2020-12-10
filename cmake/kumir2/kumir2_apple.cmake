99624d673e2505750011debc862a7d75448f7848
Alexander A. Maly <a.a.maly@gmail.com>
2020-12-11 01:53:01 +0300

    Turned off NDEBUG and QT_NO_DEBUG for Mac
    in Release mode

--- cmake/kumir2/kumir2_apple.cmake
+++ cmake/kumir2/kumir2_apple.cmake
@@ -24,7 +24,7 @@ set(KUMIR2_SDK_CMAKE_DIR "${KUMIR2_RESOURCES_DIR}/devel/cmake")
 
 # Compile flags
 set(KUMIR2_CXXFLAGS "-fno-exceptions -std=c++0x -fPIC -DAPPLE -Wall -Wno-inconsistent-missing-override -DQT_NO_DEPRECATED_WARNINGS")
-set(KUMIR2_CXXFLAGS_Release "-O2 -DNDEBUG -DQT_NO_DEBUG")
+set(KUMIR2_CXXFLAGS_Release "-O2 -UNDEBUG -UQT_NO_DEBUG")
 set(KUMIR2_CXXFLAGS_Debug "-g -O0 -Werror -Wreorder -Wreturn-type -Wno-error=unused-variable -Wno-error=unused-parameter")
 
 # Linkage flags
